0fac4ee72516458806cb993eba8ebe4d361ba95e
jpbatallon <jpb_k13@hotmail.com>
2025-08-13 20:45:31 -0300

    Se creo la carpeta Python, dentro laboratorio_usuarios con sus subcarpetas, se instaló venv y librerías, se realizó la tarea de la clase nº 1 de Python con su UML

---
+++ Python/Laboratorio_Usuarios/main.py
@@ -0,0 +1,6 @@
+from entidades.Usuario import Usuario
+from utils.menu import menu_handler
+
+if __name__ == "__main__":
+    menu_handler()
+    usuario = Usuario("Alejandro", "Administrador.123", 1)
