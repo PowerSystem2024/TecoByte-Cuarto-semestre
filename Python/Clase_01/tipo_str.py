1ff53518476aaac10fa0ccaa1955668360f206d4
Maximiliano David Tomio <maxidpoggio@gmail.com>
2025-09-16 22:37:10 -0300

    tipo_str
    tipo_str

---
+++ Python/Clase_01/tipo_str.py
@@ -0,0 +1,10 @@
+# Profundizando en el tipo String
+# Concatenación automática en Python
+
+variable = 'Adiós'
+mensaje = 'Hola ''Alumnos' + variable
+mensaje += ', Terminamos'
+# print(mensaje)
+
+# Usamos la clase help para ayuda o documentación (built-in)
+# help(math.isnan)
