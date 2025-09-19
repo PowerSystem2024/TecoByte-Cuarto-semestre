6187a368b747840183aaf30b93efb86bc8c3b745
Maximiliano David Tomio <maxidpoggio@gmail.com>
2025-09-18 22:49:29 -0300

    Clase 04
    Profundizando en string

---
+++ Python/Clase_01/mi_clase.py
@@ -0,0 +1,24 @@
+
+
+class Miclase:
+    '''
+    Este es un ejemplo de la documentación
+    de esta clase.
+    Es solo para saber que aquí está Miclase
+    '''
+    def __init__(self):
+        '''
+        Aquí está el método dunder init
+        Este método es el de Inicio
+        para esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el método creado por mi que recibbe parámetros
+        Detectó los parámetros automáticamente
+        :param param1: Este es el parámetro número 1
+        :param param2: Este es el parámetro número 2
+        return: Este es el valor de retorno
+        '''
+        
