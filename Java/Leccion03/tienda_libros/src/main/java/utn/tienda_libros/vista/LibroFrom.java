703f8b6eaf21843b09fefe83da0d38e55a9b5f81
Luciana Fragueiro <100296143+lucianafragueiro@users.noreply.github.com>
2025-10-06 09:58:09 -0300

    Clase 8 java parte 1

--- Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -1,13 +1,20 @@
 package utn.tienda_libros.vista;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.tools.Tool;
+import java.awt.*;
+
+@Component
 
 public class LibroFrom extends JFrame {
     LibroServicio libroServicio;
     private Jpanel panel;
+    private DefaultTableModel tablaModeloLibros;
 
     @Autowired
     public LibroFrom(LibroServicio libroServicio){
@@ -20,5 +27,15 @@ public class LibroFrom extends JFrame {
         setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
         setVisible(true);
         setSize(900, 700);
+        //Para obtener las dimensiones de la ventana
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension tamanioPantalla = toolkit.getScreenSize();
+        int x = (tamanioPantalla.width - getWidth()/2);
+        int y = (tamanioPantalla.height - getHeight()/2);
+        setLocation(x, y);
+    }
+    private void createUIComponents(){
+        // TODO: place custom component creation code here
+
     }
 }
