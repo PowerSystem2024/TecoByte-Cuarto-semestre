072dc1a4bd9ecc984252fc46d73057afb249e08c
jpbatallon <jpb_k13@hotmail.com>
2025-10-15 20:18:46 -0300

    Corrección de la explosión de los enemigos

--- Python/Juego_Tecnicatura/personaje.py
+++ Python/Juego_Tecnicatura/personaje.py
@@ -63,26 +63,26 @@ class Laser:
         screen.blit(self.image, self.rect.topleft)
 
 class Explosion:
-  def __init__(self,x,y):
-    # Construye la ruta completa a las imágenes de la explosión
-    self.images = [pygame.image.load(os.path.join(ASSETS_PATH, 'images', f'regularExplosion0{i:02d}.png')) for i in range (9)]
-    self.index = 0  # Indice de la animaciónp
-    self.image = self.images[self.index]  # Imágen actual
-    self.rect = self.image.get_rect(center=(x,y))  # Rectángulo de la imágen
-    self.frame_rate = 0  # Contador de los frame de la animación
-    self.max_frames = 20  # Frames por imagenes
-  
-  def actualizar(self):
-    # Actualiza la animación del disparo
-    self.frame_rate += 1
-    if self.frame_rate >= self.max_frames:
-      self.frame_rate = 0
-      self.index += 1
-      if self.index >= len(self.images):
-        return False  # Retorna en False porque termina la animación (Que va a ir de 0 -> 9)
-      self.image = self.images[self.index]
-      return True
-  
-  def dibujar(self, screen):
-    # Dibuja la imagen en la pantalla
-    screen.blit(self.image, self.rect.topleft)
+    def __init__(self, x, y):
+        # Construye la ruta completa a las imágenes de la explosión
+        self.images = [pygame.image.load(os.path.join(ASSETS_PATH, 'images', f'regularExplosion0{i:02d}.png')) for i in range(9)]
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect(center=(x, y))
+        self.frame_rate = 0  # Contador de los frames de la animación
+        self.max_frames = 5  # Menor valor = animación más fluida y visible
+
+    def actualizar(self):
+        # Actualiza la animación
+        self.frame_rate += 1
+        if self.frame_rate >= self.max_frames:
+            self.frame_rate = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                return False  # Termina la animación si se han mostrado todas las imágenes
+            self.image = self.images[self.index]
+        return True
+
+    def dibujar(self, screen):
+        # Dibuja la imagen en la pantalla
+        screen.blit(self.image, self.rect)
